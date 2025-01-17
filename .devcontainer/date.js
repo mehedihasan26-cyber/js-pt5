12bb194fdd376feb37c23c38a020d9187c623263
mehedihasan26-cyber <mehedihasan180580@gmail.com>
2025-01-17 18:58:00 +0000

    cng

---
+++ .devcontainer/date.js
@@ -0,0 +1,10 @@
+let myDate =new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+
---
+++ .devcontainer/num.js
@@ -0,0 +1,26 @@
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//tofixed (2) wuill give 2 number after point
+
+
+const otherNumber=123.89
+console.log(otherNumber.toPrecision(3));//the toprecision (3) will give converting 3 number output 124
+
+const againNum=10000000
+console.log(againNum.toLocaleString());
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min))+1);
+
+
+
+
+
+
+
